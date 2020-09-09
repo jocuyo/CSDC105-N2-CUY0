938965d530d32fb5362436de1a8f73e4b805f170
jocuyo <70056634+jocuyo@users.noreply.github.com>
2020-09-09 20:23:19 +0800

    Create cuyo_exer2.py

---
+++ exercise2/cuyo_exer2.py
@@ -0,0 +1,26 @@
+ # Author          : Josephine Cuyo
+ # Course and Year : 2nd Year BS-IT
+ # Filename        : cuyo_e2.py
+ # Description     : Python program that prints something about yourself
+ # Honor Code      : I have not given nor received any unathorized help in
+ #                   completing this exercise. I am also well aware of the 
+ #                   policies stipulated in the AdNU student handbook.
+
+
+data = {}
+data["name"] = "Josephine Cuyo"
+data["animal"] = "butterfly" 
+data["reason"] = "like a butterfly, i went through a transformation."
+data["hobby1"] = "* Play video games"
+data["hobby2"] = "* Play the guitar"
+data["hobby3"] = "* Draw in my journal"
+data["profession"] = "game developer or work in the gaming industry"
+
+print("I am",data["name"])
+print("My spirit animal is",data["animal"])
+print("because",data["reason"])
+print("When not in school, I love to:")
+print(data["hobby1"])
+print(data["hobby2"])
+print(data["hobby3"])
+print("I dream of being a/an", data["profession"], "in the future.")
