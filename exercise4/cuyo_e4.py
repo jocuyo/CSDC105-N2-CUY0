08a0c39360962fa685ee3c0806ef538fbbaabd75
jocuyo <70056634+jocuyo@users.noreply.github.com>
2020-10-04 06:30:36 +0800

    Update cuyo_e4.py

--- exercise4/cuyo_e4.py
+++ exercise4/cuyo_e4.py
@@ -1,15 +1,20 @@
 # Author          : Josephine Cuyo
 # Course and Year : 2nd Year BS-IT
 # Filename        : cuyo_e4.py
-# Description     : {A brief description of what this program does}
+# Description     : Reads 3 comma separated numbers, checks the lengths if it is valid and then computes
+#                   for the perimeter and area using the function in geometry.py and displays the results.
+#                   If the lengths given by the user is invalid, the program issues an error.
 # Honor Code      : I have not given nor received any unathorized help in
 #                   completing this exercise. I am also well aware of the 
 #                   policies stipulated in the AdNU student handbook.
 
+
+from geometry import perimeter, triangle_heronsarea
+
 def CheckingSides(a,b,c):                               
     if ((a + b) <= c) or ((b + c) <= a) or ((a + c) <= b): 
         return False                                     # function that returns True if the sides are valid, otherwise False
-    else:                                   
+    else:
         return True
 
 
@@ -25,3 +30,9 @@ if CheckingSides(a,b,c):
     print("Area: " + format(A,'.2f'))                   # prints the area in 2 decimal places
 else:
     print("Error: Invalid input")                       # if the lengths are invalid, program prints "Error: Invalid input"
+
+
+
+
+
+
