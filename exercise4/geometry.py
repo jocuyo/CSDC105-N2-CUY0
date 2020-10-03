e3e2fd7e961c6543a83415dc8de462d2adf5c49f
jocuyo <70056634+jocuyo@users.noreply.github.com>
2020-10-04 06:18:48 +0800

    Update geometry.py

--- exercise4/geometry.py
+++ exercise4/geometry.py
@@ -1,12 +1,14 @@
 # Author          : Josephine Cuyo
 # Course and Year : 2nd Year BS-IT
 # Filename        : geometry.py
-# Description     : {A brief description of what this program does}
+# Description     : The necessary code to compute the perimeter of a polygon
+#                   and the area of a triangle using Heron’s formula.
 # Honor Code      : I have not given nor received any unathorized help in
 #                   completing this exercise. I am also well aware of the 
 #                   policies stipulated in the AdNU student handbook.
 
-import math
+
+import math                                             # to use the sqrt function, we need import math
 
 def perimeter(a,b,c):       
     return a + b + c                                    # function where it returns the sum of all sides (the perimeter)
@@ -15,3 +17,5 @@ def triangle_heronsarea(a,b,c):
     s = (a+b+c)/2                                       # formula for the semi-perimeter wwhich is needed in computing the area
     heronsarea = math.sqrt(s*(s-a)*(s-b)*(s-c))         # formula in finding the area of the triangle
     return heronsarea                                   # function returns the computed value of herons area
+
+
